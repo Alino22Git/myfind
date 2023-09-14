761004f7705ff3950b1f7f285ba46914b776af2d
Alino22Git <hajra.alen@hotmail.com>
2023-09-14 19:26:31 +0200

    Added searching functionality

---
+++ README.txt
@@ -0,0 +1,9 @@
+Das Grundgerüst wurde erstellt.
+Programm funktioniert!
+Fehlende Funktionalitäten:
+-sichere Parameterauswahl mittels "getopt()" sollte realtiv einfach sein
+-try catch!
+-Forks bzw. paralleles arbeiten
+-Dokumenation
+-Testing
+-Deliverables
--- myfind.cpp
+++ myfind.cpp
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <vector>
 #include <string>
+#include <filesystem>
+#include <iostream>
+#include <algorithm>
+namespace fs = std::filesystem;
 
-int main(int argc, char *argv[])
+void StringCompare(std::string entry, std::string searchingFile, bool caseInSensitive);
+    // void outPutFormat(int processID,std::vector<std::string> files,std::vector<std::string> paths);
+
+    int main(int argc, char *argv[])
 {
-    bool reksursive = false, caseSensitive = false;
+    bool recursive = false, caseInSensitive = false;
     std::string searchPath;
     std::vector<std::string> files;
 
@@ -13,11 +20,11 @@ int main(int argc, char *argv[])
         std::string arg = argv[i];
         if (arg == "-R")
         {
-            reksursive = true;
+            recursive = true;
         }
         else if (arg == "-i")
         {
-            caseSensitive = true;
+            caseInSensitive = true;
         }
         else if (searchPath.empty())
         {
@@ -28,6 +35,60 @@ int main(int argc, char *argv[])
             files.push_back(argv[i]);
         }
     }
-    printf("Rekursive: %d\nCaseSensitive:%d\n",reksursive,caseSensitive);
+
+    if (searchPath.empty() || files.empty())
+    {
+        std::cout << "Usage: ./myfind [-R] [-i] searchpath filename1 [filename2] ... [filenameN]\n";
+        return 1;
+    }
+
+    if (recursive)
+    {
+        // for-each: you get the reference(&) of every entry in the searchpath (without & you would get a copy of the entry)
+        for (const auto &rec_entry : fs::recursive_directory_iterator(searchPath))
+        {
+            if (rec_entry.is_regular_file())
+            {
+                for (int i = 0; i < (int)files.size(); i++)
+                {
+                    StringCompare(rec_entry.path().filename().string(), files[i], caseInSensitive);
+                }
+            }
+        }
+    }
+    else
+    {
+        // for-each: you get the reference(&) of every entry in the searchpath (without & you would get a copy of the entry)
+        for (const auto &dir_entry : fs::directory_iterator(searchPath))
+        {
+            if (dir_entry.is_regular_file())
+            {
+                for (int i = 0; i < (int)files.size(); i++)
+                {
+                    StringCompare(dir_entry.path().filename().string(), files[i], caseInSensitive);
+                }
+            }
+        }
+    }
+
     return 0;
+}
+
+void StringCompare(std::string entry, std::string searchingFile, bool caseInSensitive)
+{
+    if (caseInSensitive)
+    {
+        //Testing if searchingFile exists in entry 
+        if (std::equal(entry.begin(), entry.end(), searchingFile.begin(), searchingFile.end(), [](char c1, char c2){ return std::tolower(c1) == std::tolower(c2); }))
+        {
+            std::cout << "<pid>: " << searchingFile << " : " << entry << "\n";
+        }
+    }
+    else
+    {
+    if (entry.find(searchingFile))
+    {
+        std::cout << "<pid>: " << searchingFile << " : " << entry << "\n";
+    }
+}
 }
